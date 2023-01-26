d4b53a1458539f0d1a9dd26a0e9e8a321b58d96b
psybababa <121593149+psybababa@users.noreply.github.com>
2023-01-26 19:24:13 +0900

    Create open_2ch_scraping.py

---
+++ open_2ch_scraping.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup as bs
+import cloudscraper
+import pandas as pd
+import re
+import time
+
+class scrape_onj:
+    scraper = cloudscraper.create_scraper(delay=1, browser="chrome") #cloudflareサーバーの1020エラーによるブロックを防ぐためrequestsではなくcloudscraperを用いる。尚、おーぷん2chではプロクシは使用できない。
+        
+    def get_links():
+    #スクレイピング作業をまずはおんJのスレッド一覧で行い、リンクを手に入れる。
+     links = list()
+     html_ti = scrape_onj.scraper.get('https://hayabusa.open2ch.net/headline.cgi?bbs=livejupiter') 
+     soup_ti = bs(html_ti.content,"html.parser")
+     tags = soup_ti('a')
+     for tag in tags:
+             links.append(tag.get('href',None))
+     return links
+ 
