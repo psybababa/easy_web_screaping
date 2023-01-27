7f006a94bf60becf0fece6a9ed7ea8003785dbe8
psybababa <cleanfield713@gmail.com>
2023-01-27 15:31:08 +0900

    headerでスプレッドシートに、追記時無駄な空白ができてたので更新。

--- conv_pkl_to_csv.py
+++ conv_pkl_to_csv.py
@@ -5,5 +5,5 @@ import pandas as pd
 class df_toolbox:
     def convcsv():
         df_threads = pd.read_pickle('./data/threads.pkl') 
-        df_threads.to_csv('./data/threads.csv',encoding='utf-8_sig',mode='a')
+        df_threads.to_csv('./data/threads.csv',encoding='utf-8_sig',mode='a',header=False)
         
