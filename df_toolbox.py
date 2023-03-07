c704e212cf653963722988f753a39c358062cf73
psybababa <cleanfield713@gmail.com>
2023-03-08 00:27:50 +0900

    修正 ma,filterword途中

--- df_toolbox.py
+++ df_toolbox.py
@@ -1,5 +1,6 @@
 #こっちはデータの前処理がメイン。
 import MeCab
+import matplotlib as mp
 import neologdn
 import numpy
 import os
@@ -25,7 +26,7 @@ class dfconvert:
 
 
 class  dfpreprocess:
-    #時間帯によってはスレが重複するので、それを削除する。
+    #専スレ、定期スレは重複するので、それを最初以外削除する。
     def removeemptyraw():
         fname = input('what is file name? ')
         
@@ -36,21 +37,29 @@ class  dfpreprocess:
         rawdata = pd.read_csv(f'./data/{fname}.csv')
         rawdata.drop_duplicates(inplace = True,keep = 'first')
         cleaneddata = rawdata.dropna(how = 'any')
-        cleaneddata.to_csv(f'./data/cleaned{fname}.csv',encoding = 'utf_8_sig')
+        cleaneddata.to_csv(f'./data/{fname}.csv',encoding = 'utf_8_sig')
     
     #titleとcommentを正規化する。
     def normalizetext():
         fname = input('What is file name? ')
         
-        if not os.path.exists('./data/cleaned{fname}.csv'):
-            print(f'please enter cleaned file name {fname} Maybe {fname} is not cleaned yet or does not exist')
+        if not os.path.exists('./data/{fname}.csv'):
+            print(f'Maybe {fname} does not exist')
             quit()
             
         df = pd.read_csv(f'./data/cleaned{fname}.csv')
         df['title'] = df['title'].apply(neologdn.normalize)
         df['comment'] = df['comment'].apply(neologdn.normalize)
-        df.to_csv(f'./data/normalized{fname}.csv')
+        df.to_csv(f'./data/{fname}.csv')
             
+    def makefilter():
+        df = pd.read_cdv('./data/comments.csv')
+        comments = [c for c in df['comments'] and df['comments']!= None]
+        tagger = MeCab.Tagger()
+        
+        
+        
+        
         
     #好ましくないレスをフィルターにかける。
     def filterwords():
@@ -59,10 +68,9 @@ class  dfpreprocess:
         
         fname = input('what is file name?')
         
-        if not os.path.exists(f'./data/normalized{fname}.csv'):
-            print(f'error happend while opening{fname}.csv. Maybe {fname} is not normalized yet or does not exists')
+        if not os.path.exists(f'./data/{fname}.csv'):
+            print(f'error happend while opening{fname}.csv. Maybe {fname} does not exists')
             quit()
             
-        df = pd.read_csv(f'./data/normalized{fname}.csv')
+        df = pd.read_csv(f'./data/{fname}.csv')
        
-        #ここまでかいた。次はfilterwordsを決める為に、使われた数が多い言葉の中から不適切なものを選んでいく。mecabを使って分けた後にdicとgetを使う？。
