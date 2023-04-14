abdc80914d4060764ea6ce205959dc474b000ccc
psybababa <cleanfield713@gmail.com>
2023-04-15 07:22:52 +0900

    df_toolboxを充実させた。visualizeの下準備

--- df_toolbox.py
+++ df_toolbox.py
@@ -1,11 +1,8 @@
-#こっちはデータの前処理がメイン。
-import matplotlib.pyplot as plt
-import neologdn
+from collections import Counter as ct
+from datetime import date
 import numpy as np
 import pandas as pd
-import spacy
-from spacymoji import Emoji
-import MeCab
+import string
 import sudachipy as sp
 
 class Convert:
@@ -28,28 +25,62 @@ class Convert:
         ]
 
     def convfile(self, finfo):
-        df = pd.read_pickle(f"./data/{finfo["fname"]}.pkl")
-        df.to_csv(f"./data/{finfo["fname"]}.csv", encoding=finfo["encoding"], mode=finfo["mode"])
+        df = pd.read_pickle(f"./data/{finfo['fname']}.pkl")
+        df.to_csv(f"./data/{finfo['fname']}.csv", encoding=finfo["encoding"], mode=finfo["mode"])
 
     def convall(self):
         for finfo in self.files:
             self.convfile(finfo)
 
 class process:
-    def __init__(self):
-        pass
+    dparam = {
+        'date': date.today(), #日付を単位での検索用
+        'time':None,#時間単位での検索用
+        'id':None, #追跡したい特定のidがいればここに入力
+        'num':None, #調べたいデータの数を入力すればよし
+        'thread':None, #特定のスレッド内で検索したければここにurlを入力すればいいです
+        }
+        
+    def changeparam(self):
+        if self.param:
+            self.dparam.update(self.param)
     
-    def cleantxt(self,txt):
-        pass    
-    
-    def deldup(self,finfo):
-        df = pd.read_csv(f"./data/{finfo["fname"]}")
+    def cleanfile(self, finfo):
+        df = pd.read_pickle(f"./data/{finfo['fname']}.pkl")
+        df['comment'] = df['comment'].replace('\n',' ')
+        df['comment'] = df['comment'].translate(str.maketrans('','',string.punctuation))
+        df['date'] = df['date'].replace('/','-')
+        date_str = f"{df['date']}{df['timetable']}"
+        df.drop(['date','timetable'],axis=1,inplace=True)
+        df['datetime'] = pd.to_datetime(date_str)
+        df.dropna(inplace=True, subset=['title', 'link'])
+        df.drop_duplicates(subset='title', inplace=True)
+        df = df.reindex(columns = ['comment','datetime','id','title','link'])
+        df.to_pickle(f"./data/{finfo['fname']}_clean.pkl")
     
     def tokenize(self,txt):
-        tokenizer = sp.Dictionary.create()
+        mode = sp.Tokenizer.SplitMode.B
+        tokenizer = sp.Dictionary().create(mode)
+        tokenizer.set_option("emoji", True)                
         tokens = tokenizer.tokenize(txt)
-        return [token.surface() for token in tokens]
-        
-
-class visualize:
+        return [t.surface() for t in tokens]
     
+    def countid(self,finfo,n):
+        df = pd.read_pickle(f"./data/{finfo['fname']}.pkl")
+        cid = ct(df['id'])
+        mcid = cid.most_common(n)
+        return mcid
+    
+    def countwords(self,finfo,n):
+        df = pd.read_pickle(f"./data/{finfo['word']}.pkl")
+        cwds = ct(df['word'])
+        mcwds = cwds.most_common(n)
+        return mcwds
+    
+    def purseid(self,finfo,id,date):
+        df = pd.read_pickle(f"./data/{finfo['word']}.pkl")
+        idf = df[df['datetime'].day == date & df['ID'] == id ]
+        return idf
+    
+class visualize:
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
--- scrapeonj.py
+++ scrapeonj.py
@@ -15,32 +15,34 @@ class scrape:
     }
     
     def geturls():
+        #テスト用。今現在のおんJの状況が知りたければこっちを使えばいい
         htmlll = scrape.scraper.get("https://hayabusa.open2ch.net/headline.cgi?bbs=livejupiter") 
-        soupll = bs(htmlll.content,"html.parser")
+        soupll = bs(htmlll.content,"lxml")
         tags = soupll("a")
         urls = [tag.get("href",None) for tag in tags if len(tag.get("href",None)) > 2]
-        return urls
+        return urls[1:]
 
     def searchurls():
+        #条件を決めて検索したければこちら
         defaults = {
-                "pnum":0,
-                "rpp":100,
-                "sword":0,
-                "bbs":"livejupiter",
-                "sorder":"updated",
-                "sorder2":"ninzu"
+                "pnum":0, #ヘッドラインの何ページまで追跡するか
+                "rpp":100,#スレを何個ずつ読み込むか
+                "sword":0,#検索用ワード　特定の言葉がスレタイに使われているレスだけ集めたい時はこっち
+                "bbs":"livejupiter",#おんJ以外の掲示板で同じことを行いたい場合 
+                "sorder":"updated",#更新順
+                "sorder2":"ninzu"#人の集まってるスレ順
         }
         
         urls = []
         searchurl = "https://hayabusa.open2ch.net/headline.cgi?bbs=livejupiter"
         
-        searchparams = {**defaults,**self.params}  # **でkeyとvalueを抽出して合成する
+        searchparams = {**defaults,**scrape.params}  # **でkeyとvalueを抽出して合成する
         
         encodedq = urllib.parse.urlencode(searchparams)
         url = searchurl + encodedq
         
         htmlll = scrape.scraper.get(url) 
-        soupll = bs(htmlll.content,"html.parser")
+        soupll = bs(htmlll.content,"lxml")
         tags = soupll("a")
         urls = [tag.get("href",None) for tag in tags if len(tag.get("href",None)) > 2]
         return urls
@@ -53,13 +55,13 @@ class scrape:
                         continue
                 try:
                         r = scrape.scraper.get(link)
-                        soup = bs(r.content,"html.parser")
+                        soup = bs(r.content,"lxml")
                         title = soup.h1.text
                         comments = (soup.dl.dd.text).strip()
                         icchidatas = soup.dl.dt.text
-                        date = re.search(r"\d*/\d*/\d*",icchidatas).group()
-                        timetable= re.search(r"\d*:\d*:\d*",icchidatas).group()
-                        id = re.search("ID:(....)",icchidatas).group()
+                        date = re.findall(r"\d*/\d*/\d*",icchidatas)[0]
+                        timetable= re.findall(r"\d*:\d*:\d*",icchidatas)[0]
+                        id = re.findall("ID:(\w*)",icchidatas)[0]
                         
                         row = {"title":title, "comments":comments, "date":date,"timetable":timetable,"id":id}
                         dfsource[link] = row
@@ -110,7 +112,7 @@ class scrape:
                 valtags = soup.find_all("dl",{"val":True})
                 
                 row.update({"comments": [
-                    {"comment": vtag.dd.text.strip(), "date":re.findall(r"\d*/\d*/\d*",vtag.dt.text), "timetable": re.findall(r"\d*:\d*:\d*", vtag.dt.text), "id":re.search("ID:(....)",vtag.dt.text)}
+                    {"comment": vtag.dd.text.strip(), "date":re.findall(r"\d*/\d*/\d*",vtag.dt.text)[0], "timetable": re.findall(r"\d*:\d*:\d*", vtag.dt.text)[0], "id":re.findall("ID:(\w\w\w\w)",vtag.dt.text)[0]}
                     for vtag in valtags if not re.search("!AA|imgur|http", vtag.dd.text)
                 ]})
                 
@@ -129,8 +131,8 @@ class scrape:
             soup = bs(source.content,"lxml")
             valtags = soup.find_all("dl",{"val":True}) 
             row.update({"comments": [
-                {"comment": vtag.dd.text.strip(), "date":re.findall(r"\d*/\d*/\d*",vtag.dt.text), "timetable": re.findall(r"\d*:\d*:\d*", vtag.dt.text), "id":re.search("ID:(....)",vtag.dt.text)}
-                for vtag in valtags if not re.search("!AA|imgur|http", vtag.dd.text)
+                {"comment": vtag.dd.text.strip(), "date":re.findall(r"\d*/\d*/\d*",vtag.dt.text)[0], "timetable": re.findall(r"\d*:\d*:\d*", vtag.dt.text)[0], "id":re.findall("ID:(\w\w\w\w)",vtag.dt.text)[0]}
+                for vtag in valtags if not re.search("!AA|imgur|http", vtag.dd.text) if not len(vtag.dd.text.strip()) < 2
             ]})
             
             time.sleep(1)
