07769430e2b1d9ac08708f0f2520c41373ee00b2
psybababa <cleanfield713@gmail.com>
2023-04-15 04:16:31 +0900

    optimized a little bit

--- df_toolbox.py
+++ df_toolbox.py
@@ -2,40 +2,54 @@
 import matplotlib.pyplot as plt
 import neologdn
 import numpy as np
-import os
 import pandas as pd
 import spacy
 from spacymoji import Emoji
 import MeCab
-import sudachipy
+import sudachipy as sp
 
 class Convert:
     def __init__(self):
         self.files = [
             {
-                'fname': 'comments',
-                'encoding': 'utf_8_sig',
-                'mode': 'a'
+                "fname": "comments",
+                "encoding": "utf_8_sig",
+                "mode": "a"
             },
             {
-                'fname': 'threads',
-                'encoding': 'utf_8_sig',
-                'mode': 'a'
+                "fname": "threads",
+                "encoding": "utf_8_sig",
+                "mode": "a"
             },
-            {'fname': 'titles',
-             'encoding': 'utf_8_sig',
-             'mode': 'a'
+            {"fname": "titles",
+             "encoding": "utf_8_sig",
+             "mode": "a"
             },
         ]
 
     def convfile(self, finfo):
-        df = pd.read_pickle(f'./data/{finfo["fname"]}.pkl')
-        df.to_csv(f'./data/{finfo["fname"]}.csv', encoding=finfo['encoding'], mode=finfo['mode'])
+        df = pd.read_pickle(f"./data/{finfo["fname"]}.pkl")
+        df.to_csv(f"./data/{finfo["fname"]}.csv", encoding=finfo["encoding"], mode=finfo["mode"])
 
     def convall(self):
         for finfo in self.files:
             self.convfile(finfo)
 
-class  process:
-    def __init__():
+class process:
+    def __init__(self):
+        pass
+    
+    def cleantxt(self,txt):
+        pass    
+    
+    def deldup(self,finfo):
+        df = pd.read_csv(f"./data/{finfo["fname"]}")
+    
+    def tokenize(self,txt):
+        tokenizer = sp.Dictionary.create()
+        tokens = tokenizer.tokenize(txt)
+        return [token.surface() for token in tokens]
         
+
+class visualize:
+    
--- scrapeonj.py
+++ scrapeonj.py
@@ -9,30 +9,30 @@ import urllib.parse
 
 class scrape:
         
-    scraper = cloudscraper.create_scraper(delay=1,browser = 'chrome')
+    scraper = cloudscraper.create_scraper(delay=1,browser = "chrome")
     params = {
             # 変化させたい検索用パラメータをdefaultsの様に書いてください。
     }
     
     def geturls():
-        htmlll = scrape.scraper.get('https://hayabusa.open2ch.net/headline.cgi?bbs=livejupiter') 
-        soupll = bs(htmlll.content,'html.parser')
-        tags = soupll('a')
-        urls = [tag.get('href',None) for tag in tags if len(tag.get('href',None)) > 2]
+        htmlll = scrape.scraper.get("https://hayabusa.open2ch.net/headline.cgi?bbs=livejupiter") 
+        soupll = bs(htmlll.content,"html.parser")
+        tags = soupll("a")
+        urls = [tag.get("href",None) for tag in tags if len(tag.get("href",None)) > 2]
         return urls
 
     def searchurls():
         defaults = {
-                'pnum':0,
-                'rpp':100,
-                'sword':0,
-                'bbs':'livejupiter',
-                'sorder':'updated',
-                'sorder2':'ninzu'
+                "pnum":0,
+                "rpp":100,
+                "sword":0,
+                "bbs":"livejupiter",
+                "sorder":"updated",
+                "sorder2":"ninzu"
         }
         
         urls = []
-        searchurl = 'https://hayabusa.open2ch.net/headline.cgi?bbs=livejupiter'
+        searchurl = "https://hayabusa.open2ch.net/headline.cgi?bbs=livejupiter"
         
         searchparams = {**defaults,**self.params}  # **でkeyとvalueを抽出して合成する
         
@@ -40,9 +40,9 @@ class scrape:
         url = searchurl + encodedq
         
         htmlll = scrape.scraper.get(url) 
-        soupll = bs(htmlll.content,'html.parser')
-        tags = soupll('a')
-        urls = [tag.get('href',None) for tag in tags if len(tag.get('href',None)) > 2]
+        soupll = bs(htmlll.content,"html.parser")
+        tags = soupll("a")
+        urls = [tag.get("href",None) for tag in tags if len(tag.get("href",None)) > 2]
         return urls
 
     def scanthreads():
@@ -53,27 +53,27 @@ class scrape:
                         continue
                 try:
                         r = scrape.scraper.get(link)
-                        soup = bs(r.content,'html.parser')
+                        soup = bs(r.content,"html.parser")
                         title = soup.h1.text
                         comments = (soup.dl.dd.text).strip()
                         icchidatas = soup.dl.dt.text
-                        date = re.search(r'\d*/\d*/\d*',icchidatas).group()
-                        timetable= re.search(r'\d*:\d*:\d*',icchidatas).group()
-                        id = re.search('ID:(....)',icchidatas).group()
+                        date = re.search(r"\d*/\d*/\d*",icchidatas).group()
+                        timetable= re.search(r"\d*:\d*:\d*",icchidatas).group()
+                        id = re.search("ID:(....)",icchidatas).group()
                         
-                        row = {'title':title, 'comments':comments, 'date':date,'timetable':timetable,'id':id}
+                        row = {"title":title, "comments":comments, "date":date,"timetable":timetable,"id":id}
                         dfsource[link] = row
                         
                         time.sleep(1)
                         
                 except requests.exceptions.RequestException as e:
-                        print(f'error happend while scraping{link}: {e}')
+                        print(f"error happend while scraping{link}: {e}")
                         continue
                
-        threads_data = [{'link':key, **val} for key,val in dfsource.items()]
+        threads_data = [{"link":key, **val} for key,val in dfsource.items()]
         threads_df = pd.DataFrame(threads_data)
 
-        with open('./data/threads.pkl', 'wb') as f:
+        with open("./data/threads.pkl", "wb") as f:
             threads_df.to_pickle(f)
       
                 
@@ -85,59 +85,57 @@ class scrape:
                 continue
             try:
                 r = scrape.scraper.get(link)
-                soup = bs(r.content, 'lxml')
+                soup = bs(r.content, "lxml")
                 title = soup.h1.text
-                row = {'link': link,'title': title}
+                dfsource.append({"link": link,"title": title})
                 
-                dfsource.append(row)
                 
                 time.sleep(1)
                 
             except requests.exceptions.RequestException as e:
                 print(f"Error occurred while accessing {link}: {e}")
                 continue
-    
         titlesdf = pd.DataFrame(dfsource)
     
-        with open('./data/titles.pkl', 'wb') as f:
+        with open("./data/titles.pkl", "wb") as f:
             titlesdf.to_pickle(f)
                 
         
     def getcomments():
-            titlesdf = pd.read_pickle('./data/titles.pkl').iloc[1:]
-            dfsource = [{'title': title, 'link': link} for title, link in zip(titlesdf['title'], titlesdf['link'])]
+            titlesdf = pd.read_pickle("./data/titles.pkl").iloc[1:]
+            dfsource = [{"title": title, "link": link} for title, link in zip(titlesdf["title"], titlesdf["link"])]
             for row in dfsource:
-                source = scrape.scraper.get(row['link'])
-                soup = bs(source.content, 'lxml')
-                valtags = soup.find_all('dl',{'val':True})
+                source = scrape.scraper.get(row["link"])
+                soup = bs(source.content, "lxml")
+                valtags = soup.find_all("dl",{"val":True})
                 
-                row.update({'comments': [
-                    {'comment': vtag.dd.text.strip(), 'date':re.findall(r'\d*/\d*/\d*',vtag.dt.text), 'timetable': re.findall(r'\d*:\d*:\d*', vtag.dt.text), 'id':re.search('ID:(....)',vtag.dt.text)}
-                    for vtag in valtags if not re.search('!AA|imgur|http', vtag.dd.text)
+                row.update({"comments": [
+                    {"comment": vtag.dd.text.strip(), "date":re.findall(r"\d*/\d*/\d*",vtag.dt.text), "timetable": re.findall(r"\d*:\d*:\d*", vtag.dt.text), "id":re.search("ID:(....)",vtag.dt.text)}
+                    for vtag in valtags if not re.search("!AA|imgur|http", vtag.dd.text)
                 ]})
                 
                 time.sleep(1)
         
-            commentsdf = pd.json_normalize(dfsource, ['comments'], ['title', 'link']).dropna(subset=['comment'])
+            commentsdf = pd.json_normalize(dfsource, ["comments"], ["title", "link"]).dropna(subset=["comment"])
         
-            with open('./data/comments.pkl', 'wb') as f:
+            with open("./data/comments.pkl", "wb") as f:
                 commentsdf.to_pickle(f)
 
     def jgetcomments():
-        titlesdf = pd.read_pickle('./data/titles.pkl').iloc[1:]
-        dfsource = [{'title':title, 'link':link} for title,link in zip(titlesdf['title'],titlesdf['link'])]
+        titlesdf = pd.read_pickle("./data/titles.pkl").iloc[1:]
+        dfsource = [{"title":title, "link":link} for title,link in zip(titlesdf["title"],titlesdf["link"])]
         for row in dfsource:
-            source = scrape.scraper.get(row['link'])
-            soup = bs(source.content,'lxml')
-            valtags = soup.find_all('dl',{'val':True}) 
-            row.update({'comments': [
-                {'comment': vtag.dd.text.strip(), 'date':re.findall(r'\d*/\d*/\d*',vtag.dt.text), 'timetable': re.findall(r'\d*:\d*:\d*', vtag.dt.text), 'id':re.search('ID:(....)',vtag.dt.text)}
-                for vtag in valtags if not re.search('!AA|imgur|http', vtag.dd.text)
+            source = scrape.scraper.get(row["link"])
+            soup = bs(source.content,"lxml")
+            valtags = soup.find_all("dl",{"val":True}) 
+            row.update({"comments": [
+                {"comment": vtag.dd.text.strip(), "date":re.findall(r"\d*/\d*/\d*",vtag.dt.text), "timetable": re.findall(r"\d*:\d*:\d*", vtag.dt.text), "id":re.search("ID:(....)",vtag.dt.text)}
+                for vtag in valtags if not re.search("!AA|imgur|http", vtag.dd.text)
             ]})
             
             time.sleep(1)
             
-        with open('./data/comments.json','w') as f:
+        with open("./data/comments.json","w") as f:
             json.dump(dfsource,f)        
             
 scrape.getcomments()
